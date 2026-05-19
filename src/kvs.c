aa2deec0d4ad5757e7bf6db50294960af6336051
hioken <radcloseff@gmail.com>
2026-05-19 16:57:54 +0900

    FIX README.md

--- src/kvs.c
+++ src/kvs.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 // #include <stdlib.h>
 
-#define MAX_NODES 100
+#define MAX_NODES 1000
 #define TABLE_SIZE 1009
 #define KEY_SIZE 256
 #define VALUE_SIZE 768
@@ -12,14 +12,30 @@
     #define STATIC static
 #endif
 
+typedef enum {
+    TYPE_STRING,
+    TYPE_LIST,
+    TYPE_HASH
+} DataType;
 
-typedef
-
-// STATIC Node pool[MAX_NODES]
+typedef struct Node Node;
+struct Node {
+    char key[KEY_SIZE];
+    DataType: type;
+    Node* next;
+    union {
+        char string_val[VALUE_SIZE];
+        Node* sub_node;
+    } value;
+};
 
 uint32_t hash_fnv1a(const char*);
 
+STATIC Node pool[MAX_NODES];
+STATIC Node* hash_table[TABLE_SIZE] = { NULL };
+STATIC Node* free_head = &pool[0];
 
+// 以下の関数はgeminiの丸写し
 uint32_t hash_fnv1a(const char *str) {
     uint32_t hash = 2166136261U; // FNVの32ビット用初期値
     const uint32_t fnv_prime = 16777619U; // FNVの32ビット用素数
--- src/main.c
+++ src/main.c
@@ -10,25 +10,25 @@ typedef struct {
 } CommandTable;
 
 int main(void) {
-  // kvsのsetup呼び出し
-  // ループ
-    // if (input_line()) { エラー }
-  return 0;
+    // kvsのsetup呼び出し
+    // ループ
+      // if (input_line()) { エラー }
+    return 0;
 }
 
 int input_line(void) {
-  // 入力待ち
-  // if sizeチェック エラー
-  // if (parse_input(入力)) return 1;
+    // 入力待ち
+    // if sizeチェック エラー
+    // if (parse_input(入力)) return 1;
 })
 
 int parse_input(char* input) {
-  // int flg = 1;
-  // 解析
-  // static const CommandTable
-  // ループ処理
-    // 見つかったら flg = 0;
-  // if (flg) {エラー return 1}
+    // int flg = 1;
+    // 解析
+    // static const CommandTable
+    // ループ処理
+        // 見つかったら flg = 0;
+    // if (flg) {エラー return 1}
 
-  return 0;
+    return 0;
 }
